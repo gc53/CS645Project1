5e834be2c273f6b952321c19837f369e08bc5b43
gc53 <gc53@njit.edu>
2022-02-24 10:57:20 -0500

    Optimized MT
    Cracker will now calculate hashes on relatively "small" chunks of passwords and check if any of the hashes matches the shadow file.  Also dispenses with hash map.  Significantly reduces the amount of memory that needs to be committed. Can be left unattended and will process "rockyou" in about 2 hours with an aggressive threading scheme.  A less aggressive scheme is used in this commit to not bog down the rest of the system.

--- src/cs645project1/Cracker.java
+++ src/cs645project1/Cracker.java
@@ -2,7 +2,6 @@
 package cs645project1;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -20,8 +19,12 @@ public class Cracker extends Thread{
 
     static String PWD_DIR = "common-passwords.txt";
     static String SHADOW_DIR = "shadow";
-    private static final ExecutorService workers = Executors.newCachedThreadPool();
+    static int TASK_SIZE = 1000;
+    private static final ExecutorService workers = 
+            Executors.newFixedThreadPool( Runtime.getRuntime().availableProcessors() );
+            //Executors.newCachedThreadPool();
     
+    // Deprecated original single threaded function that calculates hash for all users
     private static HashMap makePreCalcTbl( ArrayList<String> pwd_list, ArrayList<Shadow> shadow ){
         
         HashMap pre_calc_tbl = new HashMap();
@@ -49,6 +52,8 @@ public class Cracker extends Thread{
         return pre_calc_tbl;
     }
     
+    // Deprecated revised single threaded function that calcuates hashes for a single user
+    // to find their password
     private static String getPwd( ArrayList<String> pwd_list, Shadow user ){
         
         HashMap pre_calc_tbl = new HashMap();
@@ -82,56 +87,70 @@ public class Cracker extends Thread{
         }
     }
     
-    private static String getPwdMT( ArrayList<String> pwd_list, Shadow user ) throws InterruptedException, ExecutionException{
+    // Multithreaded function that calcuates hashes for a single user
+    // to find their password
+    private static String getPwdMT( ArrayList<String> pwd_list, Shadow user ) 
+            throws InterruptedException, ExecutionException{
         
-        HashMap pre_calc_tbl = new HashMap();
-        
-        Collection<Callable<String[]>> tasks = new ArrayList<>();
+        List<Callable<String[]>> tasks = new ArrayList<>();
 
         // Iterate through list of common passwords
         for( String pwd : pwd_list ){
-                
-            // MD5Shadow.crypt() does not appear to work with passwords longer than 16 characters.
-            if( pwd.length() > 16){
-                continue;
-            }
-            
 
-            tasks.add(new Callable<String[]>()
-            {
-
-            public String[] call()
-                  throws Exception
-            {
-                String[] out = new String[2];
-                out[0] = MD5Shadow.crypt( pwd, user.getSalt() );
-                out[1] = user.getUsername() + ":" + pwd;
-                return out;
-            }
-
-            });
-        }
-            
-                
-        List<Future<String[]>> results = workers.invokeAll(tasks/*, 120, TimeUnit.SECONDS*/);
-        for (Future<String[]> f : results) {
-            String[] str = f.get();
-            pre_calc_tbl.put( 
-                    str[0], //Salt & Password Hash
-                    str[1]  //Username & Password
+            // Add task to calculate and return hash of password & salt along
+            // with corresponding password to list of tasks to be run
+            tasks.add(
+                    new Callable<String[]>()
+                    {
+                        public String[] call() throws Exception
+                        {
+                            String[] out = new String[2];
+                            
+                            // Store password that will be calculated to hash.
+                            // Truncate passwords longer than 16 characters since
+                            // MD5Shadow.crypt() does not appear to work with passwords longer than that.
+                            out[0] = pwd.length() > 16 ? pwd.substring( 0, 16 ) : pwd;
+                            
+                            // Calculate hash of password & salt
+                            out[1] = MD5Shadow.crypt( out[0], user.getSalt() );
+                            
+                            
+                            
+                            return out;
+                        }
+                    }
             );
-        }    
-
-        if( pre_calc_tbl.containsKey( user.getHash() ) ){
-                    
-                // Print username and password corresponding to 
-                // hash from shadow file
-                return (String) pre_calc_tbl.get( user.getHash() );
-                    
-            }
-        else{
-            return null;
+            
         }
+        
+        // Iterate through list of tasks to get "small" chunks of tasks
+        for( int i=0; i < tasks.size(); i += TASK_SIZE ){
+            
+            int toIndex = i + TASK_SIZE - 1;
+            
+            // Run "small" chunks of list of tasks
+            List<Future<String[]>> results = workers.invokeAll(
+                    tasks.subList( i, toIndex >= tasks.size() ? tasks.size() : toIndex ), 
+                    10, TimeUnit.SECONDS
+            );
+            
+            // Iterate through finished tasks
+            for (Future<String[]> f : results) {
+                
+                // Get results from task
+                String[] str = f.get();
+                
+                // Return password if it matches the user's hash
+                if( str[1].equals( user.getHash() ) ){
+                    return user.getUsername() + ":" + str[0];
+                }
+                
+            }
+        }
+        
+        // Return null to signal no matching password was found
+        return null;
+
     }
     
     public static void main(String[] args) {
@@ -145,30 +164,36 @@ public class Cracker extends Thread{
         ArrayList<Shadow> shadow = FileToArray.toShadowArray( SHADOW_DIR );
         
 
-        // Make pre-calculated hash table
+        // Deprecated call to make pre-calculated hash table
         //HashMap pre_calc_tbl = makePreCalcTbl( pwd_list, shadow );
             
         // Iterate through list of users in shadow file
         for( Shadow user : shadow ){
             
+            // Deprecated single threaded call to get password for the given user
+            //String pwd = getPwd( pwd_list, user );
             
-            //String out = getPwd( pwd_list, user );
-            String out = null;
+            // Initialize result variable
+            String pwd = null;
 
+            // Try to get password for the given user from the list of passwords
             try {
-                out = getPwdMT( pwd_list, user );
+                pwd = getPwdMT( pwd_list, user );
 
             } catch (CancellationException | InterruptedException | ExecutionException ex) {
                 Logger.getLogger(Cracker.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-            System.out.println( out == null ? 
-                                user.getUsername() + ": Password not found" : 
-                                out );
+            // Print result
+            System.out.print( pwd == null ? 
+                                //user.getUsername() + ": Password not found" : 
+                                "" : 
+                                pwd + "\n" );
 
             
         }
         
+        // Cleanup after all users have been analyzed
         workers.shutdown();
         
     }
